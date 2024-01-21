3835b4b6a6cc7719723278eaeff5e52e0a03806c
not vincent <118132310+DatBirbDude@users.noreply.github.com>
2024-01-21 11:12:45 -0500

    My portion of app that probably breaks everything
    look at the title

---
+++ lib/assets/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color midnightGreen = const Color(0xFF093A3E);
+Color richBlack = const Color(0xFF001011);
+Color verdigris = const Color.fromARGB(255, 27, 102, 109);
+Color electricBlue = const Color(0xFF64E9EE);
+Color lightSkyBlue = const Color.fromARGB(255, 180, 213, 236);
