aad492e94642d6d033bb182875fe365d137aab2f
greenturtle537 <greenturtle537@mail.com>
2024-03-13 21:35:04 -0400

    Notefortar

--- server/edit_orgs.py
+++ server/edit_orgs.py
@@ -2,6 +2,9 @@ import json
 import os
 
 def edit_orgs(num, input):
+    # Note from greenturtle537 (Owner)
+    # Your function has spoiled. Complaing about string indices must be integers in logs.
+    # I pass num as an "int" and input as a whatever Vincent did.
     with open("orgs.json") as f:
         orgs = json.load(f)
     for key in input:
